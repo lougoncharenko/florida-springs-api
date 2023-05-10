b15e7ec3ec3a5ce2431a2e6c8a07054fb2e44ae6
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-05-10 15:35:27 -0400

    added authentication to spring routes

--- src/routes/spring.js
+++ src/routes/spring.js
@@ -6,61 +6,84 @@ const springs = require('../data/Springs')
 const Spring = require('../models/spring')
 
 /** Route to get all springs. */
-router.get('/', (req, res) => {
-    Spring.find().then((springs) => {
-        return res.json({springs})
-    })
-    .catch((err) => {
-        throw err.message
-    });
-})
+router.get('/', async (req, res) => {
+    if (req.user) {
+    try {
+        const springs = await Spring.find();
+        return res.json({ springs });
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+    } else {
+        return res.status(401).json({ message: 'You must be logged in to view the springs.' });
+    }
+});
 
 /** Route to get one spring by id. */
-router.get('/:springId', (req, res) => {
-    Spring.findOne({_id: req.params.springId})
-    .then(result => {
-        res.json(result)
-    }).catch(err => {
-        throw err.message
-    })
-})
+router.get('/:springId', async (req, res) => {
+    if (req.user) {
+        const { springId } = req.params;
+    try {
+        const spring = await Spring.findById(springId);
+        if (!spring) {
+            return res.status(404).json({ message: 'Spring data not found' });
+        }
+        return res.json({ spring });
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+    }
+    else {
+        return res.status(401).json({ message: 'You must be logged in to view a spring.' });
+    }
+});
 
 /** Route to add a new spring. */
-router.post('/', (req, res) => {
-    // POST localhost:3000/springs
-    let spring = new Spring(req.body)
-    spring.save().then(springResult => {
-        return res.json({spring: springResult})
-    }).catch((err) => {
-        throw err.message
-    })
-})
+router.post('/', async (req, res) => {
+    if (req.user) {
+        const { name, address, description, rating, entrance_fee  } = req.body;
+    try {
+        const spring = await Spring.create({ name, address, description, rating, entrance_fee });
+        return res.status(201).json({ spring });
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+    }
+    else {
+        return res.status(401).json({ message: 'You must be logged in create an spring' });
+    }
+});
+
 
 /** Route to update an existing spring */
-router.put('/:springId', (req, res) => {
-    Spring.findByIdAndUpdate(req.params.springId, req.body).then(() => {
-        return Spring.findOne({_id: req.params.springId})
-    }).then((spring) => {
-        return res.json({spring})
-    }).catch((err) => {
-        throw err.message
-    })
-})
+router.put('/:springId', async (req, res) => {
+    if (req.user) {
+    try {
+        const spring = await Spring.findByIdAndUpdate(req.params.springId, req.body, { new: true });
+        return res.json({ spring });
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+    }
+    else {
+        return res.status(401).json({ message: 'You must be logged in to update a spring' });
+    }
+    });
 
 /** Route to delete a spring. */
-router.delete('/:springId', (req, res) => {
-    Spring.findByIdAndDelete(req.params.springId).then((result) => {
-        if (result === null) {
-            return res.json({message: 'Spring does not exist.'})
+router.delete('/:springId', async (req, res) => {
+    if (req.user) {
+        try {
+            const spring = await Spring.findByIdAndDelete(req.params.springId);
+            return res.json({ spring });
         }
-        return res.json({
-            'message': 'Successfully deleted.',
-            '_id': req.params.springId
-        })
-    })
-    .catch((err) => {
-        throw err.message
-    })
-})
+        catch (err) {
+            res.status(500).json({ error: err.message })
+        }
+    } else {
+        return res.status(401).json({ message: 'You must be logged in to delete an spring.' });
+    }
+});
+
 
 module.exports = router
